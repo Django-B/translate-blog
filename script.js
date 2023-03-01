0d9cd3445db4904a6f832aa4dc61cd821160e427
django <django.ppp@gmail.com>
2023-03-01 21:11:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,51 @@
+$(function(){
+  text = $('#target').text()
+  tooltip = ".tooltip"
+  $('#target').html(markup(text))
+  
+  // $('.block').hover(
+  $(".block").mousemove( display_tooltip ).mouseleave(
+    function() {
+      $(tooltip).hide()
+    }
+  )
+
+  $(".block").click(
+    function() {
+      translate = prompt("Введите перевод для: "+$(this).text())
+      if (translate != null) {
+        $(this).attr("data", translate)
+        $(tooltip).html($(this).attr('data'))
+      }
+    }
+  )
+})
+
+function display_tooltip(cursor) {
+  data = $(this).attr('data')
+  console.log(data)
+  $(tooltip).show()
+  $(tooltip).html(data)
+  $(tooltip).css("left", (cursor.pageX+10)+"px").css("top", (cursor.pageY-20)+"px")
+}
+
+function markup(text) {
+  wrap1 = '<span class="block" data="Нет перевода">'
+  wrap2 = '</span>'
+
+  new_text = ''
+  tempString = ''
+  separators = [',', '.', ';']
+  for (i=0; i<text.length; i++) {
+    symbol = text[i]
+
+    if (separators.includes(symbol)) {
+      new_text += wrap1+tempString+wrap2+symbol
+      tempString = ''
+    }
+    else {
+      tempString += symbol
+    }
+  }
+  return new_text
+}
